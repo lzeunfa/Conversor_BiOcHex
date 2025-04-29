52b51c55c120e1d4b21391eb50cca54e0bfaa1a3
Eliseu <153873892+lzeunfa@users.noreply.github.com>
2025-04-28 21:41:49 -0300

    Update dec_bi.js

--- conversores/dec_bi.js
+++ conversores/dec_bi.js
@@ -1,48 +1,18 @@
-let numInput = document.getElementById("decimal"); // recebe o input
+let numInput = document.getElementById("decimal"); // input decimal como string
 
-let numeroBi = document.getElementById("binario");//recebe o input
+let inputBi = document.getElementById("binario");
 
-//funcao para realizar a conversao no onclick
-function converter(){
-    let numDecimal = parseInt(numInput.value);
-    let numResto = numDecimal; // resto do numero a ser processado
-    let arrayBinario = []; // array para armazenar o número binário
-    let expoente = 0; // expoente
-    let baseTotal = 2 ** expoente; // calculo das bases binarias
-    let ciclo = 1; // contagem dos ciclos da repetiçao
+function converter() {
+    let numDecimal = parseInt(numInput.value); // transforma o input em número
+    let arrayBi = [];
 
-    while (numResto > 0) { // enquanto ter numero a ser convertido
-
-        // reiniciar valores para o cálculo
-        expoente = 0;
-        baseTotal = 2 ** expoente;
-    
-        // determina o maior valor de baseTotal menor ou igual ao numResto
-        while (baseTotal <= numResto) {
-            expoente++;
-            baseTotal = 2 ** expoente;
-        }
-    
-        // ajusta o expoente para o valor correto (última base válida) se nao ele acaba ficando maior
-        expoente--;
-        baseTotal = 2 ** expoente;
-    
-        // inicializa o arrayBinario apenas no primeiro ciclo
-        if (ciclo === 1) {
-            arrayBinario = new Array(expoente + 1).fill("0"); // cria a array com zeros
-        }
-    
-        // marcar o "1" na posição correspondente
-        arrayBinario[arrayBinario.length - 1 - expoente] = "1";
-    
-        // atualizando o numResto e ciclo
-        numResto -= baseTotal;
-        ciclo++;
+    while (numDecimal > 0) {
+        let resto = numDecimal % 2;
+        arrayBi.unshift(resto); // adiciona o dígito mais significativo na frente
+        numDecimal = Math.floor(numDecimal / 2); // divide o número por 8 e arredonda para dnv calcular o resto
     }
-    
-    // converte a arrayBinario em uma string pra mostar o numero binario formado
-    let numBinario = arrayBinario.join(""); // junta os elementos da array em uma string
-    numeroBi.value = numBinario;
 
-    return;
+    let numeroBi = arrayBi.join(""); // junta os dígitos para transformar em número binário
+
+    inputBi.value = numeroBi;//mostra o resultado no input co m o seu value recebendo o resultado
 }
